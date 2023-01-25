c8b7c0093bb2bb2476217391b6b993a98716a927
durmusgulbahar <durmus.gulbahar@outlook.com>
2023-01-25 22:02:59 +0300

    fix:upload response

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { create } from 'ipfs-http-client'
+import { useState } from 'react';
+import { Buffer } from "buffer";
+
+// connect to ipfs daemon API server
+const INFURA_PROJECT_ID = "2Jj1KTbwwajeWGWACrpdmRlfrzy"
+const INFURA_API_KEY = "26d6aeb1dd16c1dad758d4d7a6f245fa"
+
+const projectId = INFURA_PROJECT_ID;
+const projectSecret = INFURA_API_KEY;
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+const client = create({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  apiPath: '/api/v0',
+  headers: {
+    authorization: auth,
+  }
+})
 
 function App() {
+
+  const [fileUrl, updateFileUrl] = useState("");
+
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const url = `https://nftmarket2.infura-ipfs.io/ipfs/${added.path}`
+      updateFileUrl(url)
+
+      console.log(fileUrl);
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>IPFS Example</h1>
+      
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      {
+        fileUrl && (
+          <img src={fileUrl} width="600px" /> 
+        )
+      }
+      <p>{fileUrl}</p>
+      </div>
+  )
 }
 
 export default App;
