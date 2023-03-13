1ce1cf00db6c1d7e5cadd09152c03706997b1fc4
durmusgulbahar <durmus.gulbahar@outlook.com>
2023-03-13 15:49:36 +0300

    metadata extension

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { create } from 'ipfs-http-client'
 import { useState } from 'react';
 import { Buffer } from "buffer";
+import { stringify } from 'querystring';
+
 
 // connect to ipfs daemon API server
 const INFURA_PROJECT_ID = "2Jj1KTbwwajeWGWACrpdmRlfrzy"
@@ -24,19 +26,47 @@ const client = create({
 function App() {
 
   const [fileUrl, updateFileUrl] = useState("");
-
+  const [metadataURL, setMetadataURL] = useState("");
+  const [formInput, updateFormInput] = useState({ name: '', description: '', price: '' })
+  
+  // upload image to the IPFS
   async function onChange(e) {
     const file = e.target.files[0]
     try {
       const added = await client.add(file)
       const url = `https://nftmarket2.infura-ipfs.io/ipfs/${added.path}`
+      
       updateFileUrl(url)
+      
+      //upload metadata to ipfs
 
-      console.log(fileUrl);
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+    
+  }
+
+  //
+  async function uploadToMetadata(){
+
+    const { name, description, price } = formInput
+    if (!name || !description || !price || !fileUrl) return
+
+    try {
+      const data = JSON.stringify({
+        name, description, price, image: fileUrl
+      })
+
+      const added = await client.add(data)
+      const url = `https://nftmarket2.infura-ipfs.io/ipfs/${added.path}`
+      setMetadataURL(url);
+
     } catch (error) {
       console.log('Error uploading file: ', error)
     }  
   }
+
+
 
   
   return (
@@ -47,12 +77,31 @@ function App() {
         type="file"
         onChange={onChange}
       />
+      <div className="w-1/2 flex flex-col pb-12">
+        <input 
+          placeholder="Asset Name"
+          className="mt-2 border rounded p-4"
+          onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+        />
+        <input
+          placeholder="Asset Price in Eth"
+          className="mt-2 border rounded p-4"
+          onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+        />
+        <button onClick={uploadToMetadata}>upload</button>
+       </div>
       {
         fileUrl && (
           <img src={fileUrl} width="600px" /> 
         )
       }
-      <p>{fileUrl}</p>
+      <p>Image url : {fileUrl}</p>
+      <p>Metadata url: {metadataURL}</p>
       </div>
   )
 }
